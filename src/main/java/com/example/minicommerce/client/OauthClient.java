16fadbad1e6c6aeed00a9aef5502256d1033a492
kimyongjun0129 <kimyongjun0129@naver.com>
2025-09-25 23:07:41 +0900

    remove(oauth) : 미사용 코드 삭제 - RequestMapping 어노테이션 삭제 - Component 어노테이션 삭제

--- src/main/java/com/example/minicommerce/client/OauthClient.java
+++ src/main/java/com/example/minicommerce/client/OauthClient.java
@@ -1,10 +1,7 @@
 package com.example.minicommerce.client;
 
-import org.springframework.stereotype.Component;
-
 import com.example.minicommerce.global.ProviderType;
 
-@Component
 public interface OauthClient {
     ProviderType provider();
     String login(String code, String state);
--- src/main/java/com/example/minicommerce/controller/OauthController.java
+++ src/main/java/com/example/minicommerce/controller/OauthController.java
@@ -2,14 +2,12 @@ package com.example.minicommerce.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.minicommerce.service.OauthService;
 
 @RestController
-@RequestMapping()
 public class OauthController {
 
     private final OauthService oauthService;
